1fa80902f9714ac0771d3549dac6d972dc9c947b
Ritesh Kumar <147717204+ritesh031@users.noreply.github.com>
2025-04-24 23:23:35 +0530

    24 April 2025

--- Unique Number III.cpp
+++ Unique Number III.cpp
@@ -23,3 +23,24 @@ class Solution {
         return -1;
     }
 };
+
+// Optimal Approach: ( Bit magic )
+// TC: O(n)
+// SC: O(1)
+
+class Solution {
+  public:
+    int getSingle(vector<int> &arr) {
+        // CodeGenius
+        int ele=0,mask,ones;
+        for(int i=0;i<32;i++){
+            ones=0;
+            mask=(1<<i);
+            for(int j=0;j<arr.size();j++){
+                if(arr[j]&mask) ones++;
+            }
+            if((ones%3) !=0) ele|=mask;
+        }
+        return ele;
+    }
+};
